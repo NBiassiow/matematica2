fd2c679df8d74ae95baaefda2f89b9b6d3bb5520
NBiassiow <166388508+NBiassiow@users.noreply.github.com>
2024-04-08 10:28:18 -0300

    matmatica2

---
+++ main.js
@@ -0,0 +1,7 @@
+const botoes=document.querySelectorAll(.botoes)
+console.log(botoes)
+for (let i=0;i<botoes.lenglh,i++){
+    botoes[l].onclick = function(){
+        botoes[i].classList.add("ativo");
+       }
+}
